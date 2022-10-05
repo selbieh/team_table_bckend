613a24bdfaba8155f369279272bc8b0663ef258e
elbieh <sayed.elbieh@yourparts.com>
2022-10-05 09:37:14 +0200

    fix filtering

--- baserow/t2/filters.py
+++ baserow/t2/filters.py
@@ -4,24 +4,25 @@ from baserow.core.action.models import Action
 
 
 class ModelContainJsonFilter(filters.FilterSet):
-    field = filters.CharFilter(method='get_field')
-    row=filters.CharFilter(method='get_row')
-    table=filters.CharFilter(method='get_table')
+    table=filters.NumberFilter(method='get_table')
+    field = filters.NumberFilter(method='get_field')
+    row=filters.NumberFilter(method='get_row')
+
+    def get_table(self, queryset, name, value):
+        if name and value:
+            return queryset.filter(scope=f'table{value}').filter(params__table_id=int(value))
+        return queryset
     def get_field(self,queryset,name,value):
 
         if name and value:
-            return queryset.filter(params__new_row_values__has_key=value)
+            return queryset.filter(params__new_row_values__has_key=int(value))
         return queryset
     def get_row(self,queryset,name,value):
 
         if name and value:
-            return queryset.filter(params__row_id=value)
+            return queryset.filter(params__row_id=int(value))
         return queryset
-    def get_table(self,queryset,name,value):
 
-        if name and value:
-            return queryset.filter(params__table_id=value)
-        return queryset
     class Meta:
         model = Action
         fields = ['field','table','row']
