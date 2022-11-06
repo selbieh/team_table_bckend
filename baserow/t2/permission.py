faff3f8115c365564a2dd9d2e5b3fea01e1e85d6
elbieh <sayed.elbieh@yourparts.com>
2022-11-06 09:33:03 +0200

    update the permissions

---
+++ baserow/t2/permission.py
@@ -0,0 +1,13 @@
+
+from rest_framework.permissions import BasePermission
+
+
+
+class IsStaff(BasePermission):
+    """
+    Allows access only to authenticated users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff)
+
--- baserow/t2/views/staff_users.py
+++ baserow/t2/views/staff_users.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
-from rest_framework_jwt.permissions import IsSuperUser
 
 from baserow.api.pagination import PageNumberPagination
+from baserow.t2.permission import IsStaff
 from baserow.t2.serializers.staff_control import StaffUserControlSerializer
 
 
 class StaffUserControlViewSet(ModelViewSet):
     serializer_class = StaffUserControlSerializer
-    permission_classes = [IsSuperUser]
+    permission_classes = [IsStaff]
 
     def get_queryset(self):
         return get_user_model().objects.all().order_by('-id')
