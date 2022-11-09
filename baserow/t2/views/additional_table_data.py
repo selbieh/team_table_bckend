dfbfaa48f62c5b11559459b0eeed6aa4ebaca9a3
elbieh <sayed.elbieh@yourparts.com>
2022-11-09 09:02:51 +0200

    add filter with user Grpup

--- baserow/t2/views/additional_table_data.py
+++ baserow/t2/views/additional_table_data.py
@@ -1,13 +1,17 @@
+
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from baserow.core.models import Group
 from baserow.t2.models import AdditionalTableData
 from baserow.t2.serializers.addtional_table_data import AdditionalTableDataSerializer
 
 
 class AdditionalTableDataView(ModelViewSet):
-    queryset = AdditionalTableData.objects.exclude(table__trashed=True).order_by('-id')
     serializer_class = AdditionalTableDataSerializer
     pagination_class = LimitOffsetPagination
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return AdditionalTableData.objects.filter(table__database__group__in=Group.objects.filter(users=self.request.user)).exclude(table__trashed=True).order_by('-id')
